148d9ab3e62a9a6eaa2080f5266d9bba13a29f3a
Monkar44 <m.kubas2009@gmail.com>
2026-03-07 22:57:59 +0100

    data validation

--- RapidPack/ParcelCalculator.cs
+++ RapidPack/ParcelCalculator.cs
@@ -4,5 +4,35 @@ namespace RapidPack;
 
 public class ParcelCalculator
 {
-    public int CalculatePrice(int ParcelHeight, int parcelDepth, int parcelWeight, bool express, int deliveryChoice)
+    public string CalculatePrice(int parcelHeight, int parcelWidth, int parcelDepth, int parcelWeight, bool express, string deliveryChoice){
+    
+        if (parcelHeight <= 0)
+        {
+            return "Wysokość musi być większa od 0!";
+        }
+        if (parcelWidth <= 0)
+        {
+            return "Szerokość musi być większa od 0!";
+        }
+        if (parcelDepth <= 0)
+        {
+            return "Głębokość musi być większa od 0!";
+        }
+
+        if (parcelWeight <= 0)
+        {
+            return "Waga musi być większa od 0!";
+        }
+
+        if (parcelWeight > 30)
+        {
+            return "Waga przesyłki nie może przekraczać 30 kg!";
+        }
+
+        if (deliveryChoice != "Standardowa" && deliveryChoice != "Ostrożnie" && deliveryChoice != "Paleta")
+        {
+            return "Nieznany typ przesyłki!";
+        }
+        
+    }
 }
